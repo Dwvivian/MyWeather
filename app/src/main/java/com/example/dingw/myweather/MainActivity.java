cf12edd97a7a0fd5eb1f7000e4c467dbd2620990
xdw <xdw@163.com>
2018-10-24 10:01:47 +0800

    修改

--- app/src/main/java/com/example/dingw/myweather/MainActivity.java
+++ app/src/main/java/com/example/dingw/myweather/MainActivity.java
@@ -9,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        //这是我修改的
+        int i =0;
+
+
     }
 }
